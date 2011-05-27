9403d864319bf391e5c9e4b2f1bb02e0d6178dce
Kibet Yegon <kibyegn@gmail.com>
2011-05-27 12:02:29 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hisa::Application.config.secret_token = '0584f4560651e6c2318f05b61ae8771254f82444f75e04cfa3ed631825db596a0e852c6adbae1f3da7660f41884d8f5e3a25794e6c4b9f1bbad6a1303a64d4d1'
